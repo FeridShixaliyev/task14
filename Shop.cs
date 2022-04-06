2b0ca625d6a0824d341bae7aed9a3b0adc04ac20
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-04-06 11:58:17 +0400

    task

---
+++ Shop.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp43
+{
+    class Shop
+    {
+        List<Order> orders = new List<Order>();
+        public void AddOrder(Order obj)
+        {
+            orders.Add(obj);
+        }
+        public void RemoveOrderByNo(int no)
+        {
+           orders.Remove(orders.Find(o => o.No == no));
+            
+        }
+        public List<Order> FilterOrderByPrice(int min,int max)
+        {
+            List<Order> filteredprice = new List<Order>();
+            var result = orders.Find(o => o.TotalAmount > min && o.TotalAmount < max);
+            filteredprice.Add(result);
+            return filteredprice;           
+        }
+        public int GetOrdersAvg(DateTime date)
+        {
+            List<Order> filteredprice = new List<Order>();
+
+            filteredprice.AddRange(orders.FindAll(o => o.CreatedAt.Date.Hour>= date.Hour));
+            int sum = 0;
+            foreach (var item in filteredprice)
+            {
+                sum += item.TotalAmount;
+            }
+            return sum/ filteredprice.Count;
+        }
+        public int GetOrdersAvg()
+        {
+            int sum = 0;
+            foreach (var item in orders)
+            {
+                sum += item.TotalAmount;
+            }
+                sum /= orders.Count;
+            return sum;
+        }
+    }
+}

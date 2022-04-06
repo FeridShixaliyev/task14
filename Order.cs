d93a14966572f0a503f1f0a555baeb3ffcf1e216
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-04-06 11:57:50 +0400

    task

---
+++ Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp43
+{
+    class Order
+    {
+        public Order()
+        {
+            _createdAt = DateTime.UtcNow.Date;
+            CreatedAt = _createdAt;
+            _no++;
+            No = _no;
+        }
+        private static DateTime _createdAt;
+        private static int _no;
+        public int No { get; set; }
+        public int ProductCount { get; set; }
+        public int TotalAmount { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
